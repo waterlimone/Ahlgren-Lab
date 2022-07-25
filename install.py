dada47acf081349f5c73158ee39847d0b54194d1
Cameron McIlvenna <40866507+waterlimone@users.noreply.github.com>
2022-07-25 18:41:50 -0400

    Delete install.py

--- install.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print("----------------\nInstalling Conda\n----------------")
-os.system("./condaInstall.sh")
-
-print("----------------------\nInstalling Environment\n----------------------")
-os.system("./environmentInstall.sh")
