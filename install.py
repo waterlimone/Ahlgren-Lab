f30b389de4e888023d366562873f9dad50a02291
Cameron McIlvenna <cmcilvenna@master.cl.clarku.edu>
2022-07-25 12:37:11 -0400

    Fixed install script.

---
+++ install.py
@@ -0,0 +1,7 @@
+import os
+
+print("----------------\nInstalling Conda\n----------------")
+os.system("./condaInstall.sh")
+
+print("----------------------\nInstalling Environment\n----------------------")
+os.system("./environmentInstall.sh")
