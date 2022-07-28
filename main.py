abb6a4e855a5552bae3619b9dc42a0fc83a45c68
Cameron McIlvenna <cmcilvenna@master.cl.clarku.edu>
2022-07-28 12:16:56 -0400

    Added a main file and added a gitignore for python compilations.

---
+++ main.py
@@ -0,0 +1,6 @@
+from readPercentage import cut_adapt_trim_log
+
+def main():
+    RWU_SYNITS = cut_adapt_trim_log("/home/cmcilvenna/RWU_amplicon_data/RWU_SYNITS_0.22filterCells_sequenceData/qual22_trimmed/trimLog")
+
+main()
