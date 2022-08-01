ce5fc79e1cacc11b7f9ad5665a184b234baa0db1
waterlimone <cmcilvenna99@gmail.com>
2022-08-01 11:50:21 -0400

    Added bin folder

--- environmentInstall.sh
+++
@@ -1,9 +0,0 @@
-# Installs wget for qiime2 install
-mamba install wget --yes
-sleep 1
-
-# Installs qiime2 as qiime2 env.
-mamba env create -n qiime2-2022.2 --file qiime2-2022.2-py38-linux-conda.yml
-
-conda init bash
-exec bash
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ___________________________
-echo Installing lab environment.
-echo ---------------------------
-
-./condaInstall.sh
-
-./environmentInstall.sh
